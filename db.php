828e65e3ba340198264049a0c752a17dbbf152d4
hatahilaa <hatahila@gmail.com>
2018-06-05 14:22:27 +0200

    DB settings changes

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "Hatahila@2017";
-$mysql_database = "ciment_mgt_db";
+$mysql_user = "user";
+$mysql_password = "**************";
+$mysql_database = "**********";
 $bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password) 
 or die("Opps some thing went wrong");
 mysqli_select_db($bd,$mysql_database) or die("Opps some thing went wrong");
